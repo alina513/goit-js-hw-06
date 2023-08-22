e31321f34f39460dd04d8d8626399e9935fc7d3a
alina513 <alinaromanko513@gmail.com>
2023-08-22 13:56:57 +0300

    fgg
    d

--- js/task5.js
+++
@@ -1,5 +0,0 @@
-let input = document.querySelector("#name-input");
-let span = document.querySelector("#name-output");
-const onInput = event => {
-    return span.textContent = event.currentTarget.value};
-input.addEventListener("input", onInput);
--- js/task7.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector("#font-size-control");
-console.dir(input.value);
-const span = document.querySelector("#text");
-const onInput = event => {
-    span.style.fontSize = `${input.value}px`;
-}
-input.addEventListener("input", onInput);
