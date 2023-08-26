4b5532eb390a7fe36f79a2317f93f1fcb22e45e7
alina513 <alinaromanko513@gmail.com>
2023-08-26 12:47:50 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,5 +1,13 @@
+// 
 let input = document.querySelector("#name-input");
 let span = document.querySelector("#name-output");
+
 const onInput = event => {
-    return span.textContent = event.currentTarget.value};
+    if (input.value.length === 0) {
+        span.textContent = "Anonymous";
+    } else {
+        span.textContent = event.currentTarget.value;
+    }
+};
+
 input.addEventListener("input", onInput);
